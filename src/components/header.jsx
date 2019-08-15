6cb364c1655d5e93eac9288f6a6aa7e08fc802fc
Edwin Estrada <5884719+edwinestrada@users.noreply.github.com>
2019-08-15 05:35:42 -0400

    Use React.Fragment shorthand and refactor face hover

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,29 +9,23 @@ class Header extends Component {
 
   handleEnterFace = () => this.setState({ faceHover: true });
   handleExitFace = () => this.setState({ faceHover: false });
+  getFace = () => this.state.faceHover ? edwinFace : manFace;
 
   render() {
-    return (
-      <div>
-        <p id="nombre-grande" className="m0 mt4">Hola</p>
-        {/* <p>I'm Edwin.</p> */}
-        <div className="clearfix py3 hide-on-desktop">
-          <p>Just a bad hombre from Raleigh, NC.</p>
-        </div>
-        <div className="clearfix py3 hide-on-mobile">
-          {/* <p className="col col-4">Just a bad hombre.</p> */}
-          <p className="col col-4">I'm Edwin.</p>
-          {/* <p className="col col-8 px3">Based in Raleigh, NC.</p> */}
-        </div>
-        <div className="clearfix">
-          {
-            this.state.faceHover ?
-            <img onMouseEnter={this.handleEnterFace} onMouseLeave={this.handleExitFace} alt="edwin's face" id="mi-cabeza-de-verdad" className="col sm-col-2 md-col-3 lg-col-1" src={edwinFace} /> :
-            <img onMouseEnter={this.handleEnterFace} onMouseLeave={this.handleExitFace} alt="cartoon face" id="mi-cabeza" className="col sm-col-2 md-col-3 lg-col-1" src={manFace} />
-          }
-        </div>
+    return (<>
+      <p id="nombre-grande" className="m0 mt4">Hola</p>
+
+      <ul class="list-reset">
+        <li>I'm Edwin,</li>
+        <li>startup tech founder</li>
+        <li>turned product manager</li>
+        <li>without a workplace to call home.</li>
+      </ul>
+
+      <div className="clearfix">
+        <img onMouseEnter={this.handleEnterFace} onMouseLeave={this.handleExitFace} alt="face" className="col sm-col-2 md-col-3 lg-col-1" src={this.getFace()} />
       </div>
-    );
+    </>);
   }
 }
 
