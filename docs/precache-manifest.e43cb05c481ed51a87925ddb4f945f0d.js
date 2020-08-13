998c6ff54206054e75ef08814d85e3fdfa10f75c
Edwin Estrada <5884719+edwinestrada@users.noreply.github.com>
2020-08-12 20:23:15 -0400

    Add new build files

---
+++ docs/precache-manifest.e43cb05c481ed51a87925ddb4f945f0d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9b6d5dfefe7e2e5ec9f6c43780075f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d1aa89e59c9a744f02a",
+    "url": "/static/css/main.fbfcae10.chunk.css"
+  },
+  {
+    "revision": "2c05295acc4a9b5a297b",
+    "url": "/static/js/2.4c79bacc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c79bacc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d1aa89e59c9a744f02a",
+    "url": "/static/js/main.2234aef3.chunk.js"
+  },
+  {
+    "revision": "487de4d6fec5b556df55",
+    "url": "/static/js/runtime-main.26c42324.js"
+  },
+  {
+    "revision": "ebaec0ca791ec29bda6d3a81c7bd11ad",
+    "url": "/static/media/bridge-logo.ebaec0ca.png"
+  },
+  {
+    "revision": "de29753e4475fc16ad0311158c6d35ce",
+    "url": "/static/media/edwin-face.de29753e.png"
+  },
+  {
+    "revision": "f596c2bfa3b74abef1e9dde5b77b8d44",
+    "url": "/static/media/i-am-a-god-or-a-dog.f596c2bf.png"
+  },
+  {
+    "revision": "df6aa15a6797988d7ab28b2b24026de5",
+    "url": "/static/media/man-face.df6aa15a.png"
+  },
+  {
+    "revision": "6810f2c5ed0d6ef18713106424a71778",
+    "url": "/static/media/pixbit-logo.6810f2c5.png"
+  }
+]);
